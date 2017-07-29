9aeb296614ff70f20c52d4c0f319e939e20317b1
Wolfram Richter <wrichter@redhat.com>
2017-07-29 17:16:53 +0200

    changed port to 8080

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
