4580fb34434818c1b1047440f309d5bea9f50a64
Wolfram Richter <wrichter@redhat.com>
2017-07-29 17:13:52 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const SocketServer = require('ws').Server;
+const stompit = require('stompit');
+const topic = { destination: '/topic/SampleTopic' };
+const wss = new SocketServer({ server });
+
+stompit.connect({ host: 'localhost', port: 61613 }, (err, stompclient) => {
+  stompclient.subscribe(topic, (err, msg) => {
+    //msg.readString('UTF-8', (err, body) => {
+      console.log('sending: %s', msg);
+      wss.clients.forEach((websocketclient) => { websocketclient.send(msg); });
+    //});
+  });
+
+  wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
+    ws.on('message', function incoming(msg) {
+      console.log('received: %s', msg);
+      const frame = stompclient.send(topic);
+      frame.write(msg);
+      frame.end();
+    });
+  });
+
+  process.on('exit', function () { //on 'SIGTERM'
+    wss.clients.forEach((websocketclient) => { websocketclient.send(msg); });
+    stompclient.disconnect();
+  });
+});
